bf70f88dfef92b3bee96b8d28f151b740eb05f99
Shubham <kr.shubham025@gmail.com>
2020-09-15 23:30:12 +0530

    add content

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 function Accordion({ items }) {
-  return (
-    <div>
-      Accordion
-      {items.length}
-    </div>
-  );
+  const renderedItems = items.map((item) => {
+    return (
+      <div key={item.title}>
+        <div className="title active">
+          <i className="dropdown item"></i>
+          {item.title}
+        </div>
+        <div className="content active">{item.content}</div>
+      </div>
+    );
+  });
+
+  return <div>{renderedItems} </div>;
 }
 
 export default Accordion;
