cc75f6647b96b767de31f383db97b69eeaffe6a3
Shubham <kr.shubham025@gmail.com>
2020-09-15 23:58:17 +0530

    Fix clasname of semantic

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,13 +3,13 @@ import React from "react";
 function Accordion({ items }) {
   const renderedItems = items.map((item) => {
     return (
-      <div key={item.title}>
+      <React.Fragment key={item.title}>
         <div className="title active">
-          <i className="dropdown item"></i>
+          <i className="dropdown icon"></i>
           {item.title}
         </div>
         <div className="content active">{item.content}</div>
-      </div>
+      </React.Fragment>
     );
   });
 
