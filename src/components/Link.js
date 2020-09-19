1d57c3360e1c1793886cc8ec262af9b3a19a317b
Shubham <kr.shubham025@gmail.com>
2020-09-19 14:27:09 +0530

    Linkjs Complete

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function Link({ href, className, children }) {
   const onClick = (event) => {
+    if (event.ctrlKey || event.metaKey) {
+      return;
+    }
     event.preventDefault();
+    window.history.pushState({}, "", href);
+
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
   };
   return (
     <a onClick={onClick} href={href} className={className}>
